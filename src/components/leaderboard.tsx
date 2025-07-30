7fc2029b1f7c43aa7ee733c86d1cb62520ff4b02
LampByLit <k.ryan.hartley@gmail.com>
2025-07-30 12:06:26 -0700

    Remove warning text from top of page

--- src/components/leaderboard.tsx
+++ src/components/leaderboard.tsx
@@ -55,22 +55,7 @@ export function Leaderboard({ data }: LeaderboardProps) {
           </a>
         </div>
         <p className="text-gray-600 text-justify max-w-4xl mx-auto leading-relaxed text-sm">
-          This leaderboard uses several exhaustive methods to scrape Amazon paperback links and sort them by Best Sellers Rank. Amazon employs very strict anti-bot measures. This{' '}
-          <button
-            onClick={handleRunCycle}
-            disabled={isRunningCycle}
-            className="inline text-gray-600 cursor-default"
-            style={{ 
-              cursor: 'default',
-              userSelect: 'text',
-              WebkitUserSelect: 'text',
-              MozUserSelect: 'text',
-              msUserSelect: 'text'
-            }}
-          >
-            tool
-          </button>
-          {' '}could break at any moment. If you want to add your book to the leaderboard,{' '}
+          This leaderboard uses several exhaustive methods to scrape Amazon paperback links and sort them by Best Sellers Rank. If you want to add your book to the leaderboard,{' '}
           <a 
             href="https://x.com/recapitul8r" 
             target="_blank" 
